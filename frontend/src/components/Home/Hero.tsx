4f3f8c5eba1e2d4e8c1f627034b3d14be0673d4a
Manish Suthar <manishsuthar078@gmail.com>
2024-09-03 20:45:34 +0530

    ...

--- frontend/src/components/Home/Hero.tsx
+++ frontend/src/components/Home/Hero.tsx
@@ -2,7 +2,6 @@ import HeroGlobe from "./HeroGlobe";
 import { HeroHighlight, Highlight } from "../ui/hero-highlight";
 import { motion } from "framer-motion";
 import { IconMouse } from "@tabler/icons-react";
-import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
@@ -30,13 +29,6 @@ const Hero = () => {
             <Highlight className="text-black dark:text-white">
               Collaborite
             </Highlight>
-            <br />
-            <Link
-              className="text-lg bg-indigo-500 py-2 px-4 font-normal"
-              to="/room/id"
-            >
-              Playground
-            </Link>
           </motion.h1>
           <HeroGlobe />
         </div>
