c3f4b5c7b79f0093a7b11a4b95988669b6c30d37
Manish Suthar <manishsuthar078@gmail.com>
2024-11-28 03:02:36 +0530

    chore: classname fixed

--- frontend/src/components/Cards/FeatureCard.tsx
+++ frontend/src/components/Cards/FeatureCard.tsx
@@ -14,7 +14,7 @@ const FeatureCard = ({
   return (
     <div
       className={cn(
-        "flex flex-col lg:border-r  py-10 relative group/feature dark:border-neutral-800",
+        "flex flex-col lg:border-r flex py-10 relative group/feature dark:border-neutral-800",
         (index === 0 || index === 4) && "lg:border-l dark:border-neutral-800",
         index < 4 && "lg:border-b dark:border-neutral-800",
       )}
