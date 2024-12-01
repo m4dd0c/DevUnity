aa254b03f06da9710408f49a43e7e5a9a52bc519
Manish Suthar <manishsuthar078@gmail.com>
2024-12-01 15:18:43 +0530

    feat: husky and lint-staged added

---
+++ frontend/lint-staged.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "*.jsx": ["npx eslint --fix", "prettier --write"],
+  "*.js": ["npx eslint --fix", "prettier --write"],
+  "*.tsx": ["npx eslint --fix", "prettier --write", "tsc --noEmit"],
+  "*.ts": ["npx eslint --fix", "prettier --write", "tsc --noEmit"],
+  "*.css": ["prettier --write"],
+  "*.json": ["prettier --write"],
+};
--- lint-staged.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	"*.jsx": ["eslint --fix", "prettier --write"],
-	"*.js": ["eslint --fix", "prettier --write"],
-	"*.tsx": ["eslint --fix", "prettier --write", "tsc --noEmit"],
-	"*.ts": ["eslint --fix", "prettier --write", "tsc --noEmit"],
-	"*.css": ["prettier --write"],
-	"*.json": ["prettier --write"],
-};
