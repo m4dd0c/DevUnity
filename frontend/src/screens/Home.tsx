481e1194cbdeb06a8528fb5488060a92e55251a6
Manish Suthar <manishsuthar078@gmail.com>
2025-04-02 17:45:52 +0530

    chore: preloading team members images

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -1,10 +1,9 @@
-import React, { Suspense, useEffect } from "react";
+import React, { Suspense } from "react";
 import { TracingBeam } from "../components/ui/tracing-beam.tsx";
 import Features from "../components/Home/Features";
 import Hero from "../components/Home/Hero";
 import Testimonials from "../components/Home/Testimonials";
 import Brands from "../components/Home/Brands.tsx";
-// import Team from "../components/Home/Team.tsx";
 import RoomPromo from "../components/Home/RoomPromo.tsx";
 import SEO from "../components/layout/SEO.tsx";
 
@@ -13,10 +12,6 @@ const Team = React.lazy(
 );
 
 function Home() {
-  useEffect(() => {
-    // Preload the Team component
-    import("../components/Home/Team");
-  }, []);
   return (
     <>
       {/* SEO - INVISIBLE IN PAGE */}
