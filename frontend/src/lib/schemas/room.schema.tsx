cb93f366a2407c14c7753bdee39461a4ea826b6a
Manish Suthar <manishsuthar078@gmail.com>
2025-04-03 22:28:24 +0530

    impr: better error message in room-forms

--- frontend/src/lib/schemas/room.schema.tsx
+++ frontend/src/lib/schemas/room.schema.tsx
@@ -3,13 +3,18 @@ import { roomIdRegex } from "../utils";
 
 export const JoinRoomSchema = z.object({
   // must be a valid UUID
-  roomId: z.string().regex(roomIdRegex),
+  roomId: z.string().regex(roomIdRegex, "Room ID must be a valid UUID."),
   password: z.string().max(100),
 });
 
 export const CreateRoomSchema = z.object({
   // must be a valid UUID
-  roomId: z.string().regex(roomIdRegex),
+  roomId: z
+    .string()
+    .regex(
+      roomIdRegex,
+      "Room ID must be a valid UUID. You can generate one using helper button below.",
+    ),
   title: z.string().min(3).max(50),
   password: z.string().min(6).max(100),
 });
@@ -26,11 +31,11 @@ export const UpdatePassAndLangSchema = z.object({
 
 export const UpdateRoomCodeSchema = z.object({
   // objectId
-  room_id: z.string().regex(roomIdRegex),
+  room_id: z.string().regex(roomIdRegex, "Room ID must be a valid UUID."),
   code: z.string().max(150000),
 });
 
 export const DeleteRoomSchema = z.object({
   // objectId
-  room_id: z.string().regex(roomIdRegex),
+  room_id: z.string().regex(roomIdRegex, "Room ID must be a valid UUID."),
 });
